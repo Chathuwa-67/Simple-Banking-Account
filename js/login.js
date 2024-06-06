c998a62c02e2795b55b075429e61af2e5e181680
Gihan Chathuranga Gunathilaka <141658614+Chathuwa-67@users.noreply.github.com>
2024-06-06 22:45:00 +0530

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,27 @@
+
+function showPass() {
+    var show = document.getElementById('user-password')
+    if (show.type == 'password') {
+        show.type = 'text';
+    }
+    else (
+        show.type = 'password'
+    )
+}
+ 
+document.getElementById('btn-submit').addEventListener('click', function () {
+
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    console.log(email);
+   
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value
+
+    if (email === 'redfox@tahmid.com' && password === 'secret') {
+        window.location.href = 'bank.html'
+    }
+    else (
+        alert("Your Password is Wrong")
+    )
+})
